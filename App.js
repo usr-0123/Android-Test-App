92314559e571e99f349490aebc50aa1bbd5291de
usr-0123 <fixedzone365@gmail.com>
2024-04-19 17:18:11 +0300

    First commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is my first android app development using react native</Text>
       <StatusBar style="auto" />
     </View>
   );
